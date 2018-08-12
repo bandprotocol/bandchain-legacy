98eccd0f164fe210ba8b53594421a9fb0382461f
Sorawit Suriyakarn <thepsint@gmail.com>
2018-08-11 21:07:02 -0400

    Add third party folder into project

--- src/state/output.h
+++ src/state/output.h
@@ -1,30 +1,12 @@
-// #pragma once
+#pragma once
 
-// #include "tx/message.h"
-// #include "util/bytes.h"
+class TxOutput
+{
+public:
+  TxOutput() = default;
+  TxOutput(const TxOutput& txo) = default;
 
-// class Output
-// {
-// public:
-//   Output(Bytes<32> ident_)
-//       : Output(ident_, {}, 0)
-//   {
-//   }
-//   Output(Bytes<32> ident_, Bytes<20> owner_, uint64_t amount_)
-//       : ident(ident_)
-//       , owner(owner_)
-//       , amount(amount_)
-//   {
-//   }
-
-//   std::string to_string() const
-//   {
-//     return "Output[ident={}, owner={}, amount={}]"_format(ident, owner,
-//     amount);
-//   }
-
-// public:
-//   const Bytes<20> owner;
-//   const uint64_t nonce = 0;
-//   const uint64_t amount = 0;
-// };
+private:
+  // Address owner;   //< Who owns this TxOutput?
+  // uint64_t amount; //< How much is this?
+};
