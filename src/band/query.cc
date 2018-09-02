91721977b7b13884ace7f4a2f28c1bf45dea2cc9
Sorawit Suriyakarn <thepsint@gmail.com>
2018-09-02 12:05:28 +0700

    Query: Implement dispatcher based on JSON method field
    Summary:
    This allows query to dispatch different query handlers
    based on "method" field. More code to come.
    
    Test Plan:
    [x] run it locally
    ```
    ⇒  curl --data-binary '{"jsonrpc":"2.0","id":"hello","method":"abci_query","params":{"data":"7B226D6574686F64223A202262616C616E6365222C2022706172616D73223A207B7D7D"}}' localhost:26657/
    {
      "jsonrpc": "2.0",
      "id": "hello",
      "result": {
        "response": {
          "value": "VE9ETw==",
          "height": "7"
        }
      }
    }%
    ```
    Note that `7B226...` is the hex encoding of `{"method": "balance", "params": {}}` and `VE9ETw==` is the base 64
    encoding of `TODO`
    
    Reviewers: bunoi
    
    Reviewed By: bunoi
    
    Subscribers: smiled0g
    
    Differential Revision: http://code.bandprotocol.com/D1

--- src/band/query.cc
+++ src/band/query.cc
@@ -1,18 +1,39 @@
 #include "query.h"
 
-#include "util/json.h"
-
 Query::Query(Context& _ctx)
     : ctx(_ctx)
 {
+  dispatcher["balance"] = &Query::process_balance;
 }
 
 std::string Query::process_query(const std::string& raw_data)
 {
   try {
     auto data = json::parse(raw_data);
-    return data.dump();
-  } catch (nlohmann::detail::parse_error&) {
+    if (!data.is_object())
+      throw Error("Invalid query data. Must be JSON object.");
+
+    auto method = data["method"];
+    if (!method.is_string())
+      throw Error("Invalid query method. Must be JSON string.");
+
+    auto params = data["params"];
+    if (!params.is_object())
+      throw Error("Invalid query params. Must be JSON object");
+
+    if (auto it = dispatcher.find(method); it != dispatcher.end()) {
+      return it->second(*this, params);
+    } else {
+      throw Error("Unknown query method");
+    }
+
+  } catch (const nlohmann::detail::parse_error&) {
     throw Error("JSON parse error");
   }
+}
+
+std::string Query::process_balance(const json& params)
+{
+  // TODO
+  return "TODO";
 }
--- src/band/query.h
+++ src/band/query.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "store/context.h"
+#include "util/json.h"
 
 class Query
 {
@@ -11,5 +12,14 @@ public:
   std::string process_query(const std::string& raw_data);
 
 private:
+  std::string process_balance(const json& params);
+
+private:
+  /// Reference to the primary blockchain context.
   Context& ctx;
+
+  /// Dispatch from method name to the corresponding processing function.
+  std::unordered_map<std::string,
+                     std::function<std::string(Query&, const json&)>>
+      dispatcher;
 };
