b6568460bb9ae65d3ab988203c7812044102dd80
Sorawit Suriyakarn <thepsint@gmail.com>
2018-10-27 15:13:32 +0700

    LICENSE: Add license header on top of all source files
    Summary: ibid
    
    Test Plan: build
    
    Reviewers: bunoi
    
    Differential Revision: http://code.bandprotocol.com/D127

--- src/contract/tcr.cc
+++ src/contract/tcr.cc
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the LICENSE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 #include "tcr.h"
 
 #include "contract/token.h"
@@ -228,7 +245,7 @@ uint256_t Registry::determine_reward(const uint256_t& challenge_id) const
   uint256_t poll_id = +m_challenges[challenge_id].poll_id;
   assert_con(vote.is_poll_ended(poll_id), "Vote havn't ended yet.");
 
-    // No people vote
+  // No people vote
   if (vote.get_total_winning_token(poll_id) == 0) {
     return 2 * +m_challenges[challenge_id].stake;
   }
